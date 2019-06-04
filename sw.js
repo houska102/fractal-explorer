df575da2caa2e848964e61f9f7f8007554bd56cb
Jan Hrubý <jan.hruby@lemonbone.cz>
2019-06-04 14:41:49 +0200

    implemented basic serviceworker <> client comunication and optimised the way fractal is drawn onto the canvas

---
+++ sw.js
@@ -0,0 +1,43 @@
+function MandelbrotTest (cx, cy, defaultzx, defaultzy) {
+  let zx,zy,zx2,zy2;    // komplexní proměnná Z
+  let iter;                   // počet iterací
+  zx=0;                               // vynulovat komplexní proměnnou C
+  zy=0;
+  const maxiter = 255
+  const colorUnit = 255/maxiter
+  iter=0;                           // nastavit počitadlo iterací
+
+  while (iter<maxiter) {                                // iterační smyčka
+      zx2=zx*zx;                      // zx^2
+      zy2=zy*zy;                      // zy^2
+      zy=2.0*zx*zy+cy;
+      zx=zx2-zy2+cx;                  // z:=z^2+c
+      iter++;
+      if((zx2+zy2)>4.0){
+        break;
+      }        // zvýšit hodnotu počitadla iterací
+  }
+  if (iter==maxiter){                  // bod leží uvnitř Mandelbrotovy množiny
+    return 0
+  } else {
+    return colorUnit*iter
+  }                               // bod leží vně Mandelbrotovy množiny
+}
+
+
+self.addEventListener("message", (event) => {
+  var data = JSON.parse(event.data);
+  const fractalInfo = data.fractalInfo
+  let shiftX = data.shiftX
+  let shiftY = data.shiftY
+  let enlarge = data.enlarge
+  let result = []
+  for (let x = 0; x<fractalInfo.width; x++) {
+    result.push(new Array(fractalInfo.width))
+    for (let y = 0; y<fractalInfo.height; y++) {
+      const color = MandelbrotTest((x+shiftX)/enlarge, (y+shiftY)/enlarge ,fractalInfo.defaultzx, fractalInfo.defaultzy)
+      result[x][y] = color
+    }
+  }
+  event.source.postMessage(result)
+});
