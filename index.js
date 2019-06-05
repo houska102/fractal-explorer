917bc86e97c146a83ecab322d426fc96f441d245
Jan Hrubý <jan.hruby@lemonbone.cz>
2019-06-05 09:48:30 +0200

    canged the syntax for inline functions to ES5

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   if(filename === './'){
     filename = './index.html'
   }
   console.log(filename)
-  fs.readFile(filename, function(err, data) {
+  fs.readFile(filename,(err, data) => {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
